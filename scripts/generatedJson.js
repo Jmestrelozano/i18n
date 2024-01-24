ebe1819d1fd27acc4b44411cd16969408c5b057b
Jmestrelozano <jmestrelozano@gmail.com>
2024-01-24 13:25:45 -0500

    implement api

--- scripts/generatedJson.js
+++ scripts/generatedJson.js
@@ -2,28 +2,6 @@
 import fs from "fs";
 import path from "path";
 
-// const data = {
-//   name: "Ejemplo",
-//   description: "Archivo JSON generado desde un script en React",
-// };
-
-// // Ruta del directorio y del archivo JSON
-// const directoryPath = "src/translations/en";
-// const filePath = path.join(directoryPath, "generated-data.json");
-
-// // Asegurarse de que el directorio exista
-// if (!fs.existsSync(directoryPath)) {
-//   fs.mkdirSync(directoryPath, { recursive: true });
-// }
-
-// // Convertir datos a formato JSON
-// const jsonData = JSON.stringify(data, null, 2);
-
-// // Escribir datos en el archivo JSON
-// fs.writeFileSync(filePath, jsonData);
-
-// console.log(`Archivo JSON generado en: ${filePath}`);
-
 const translationsArray = [
   {
     language: "en",
@@ -49,40 +27,53 @@ const translationsArray = [
   // Agrega los objetos que quieras
 ];
 
+const getTranslations = async () => {
+  const response = await fetch("https://api-i18n.vercel.app/translations");
+
+  if (response.status === 200) {
+    const result = await response.json();
+
+    return result;
+  }
+  return [];
+};
+
+getTranslations().then((resp) => {
+  // Limpiar y generar archivos JSON
+  resp.data.forEach((translation) => {
+    const { language } = translation;
+    const directoryPath = path.join(baseDirectory, language);
+    const filePath = path.join(directoryPath, "global.json");
+
+    // Asegurarse de que el directorio exista
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    // Escribir un objeto JSON vacío en el archivo
+    fs.writeFileSync(filePath, "{}", "utf-8");
+
+    console.log(`Archivo JSON limpiado para ${language} en: ${filePath}`);
+  });
+
+  // Crear carpetas para cada idioma y generar archivos JSON
+  resp.data.forEach((translation) => {
+    const { language, ...rest } = translation;
+    const directoryPath = path.join(baseDirectory, language);
+    const filePath = path.join(directoryPath, "global.json");
+
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    // Convertir datos a formato JSON
+    const jsonData = JSON.stringify({ ...rest }, null, 2);
+
+    // Escribir datos en el archivo JSON
+    fs.writeFileSync(filePath, jsonData);
+
+    console.log(`Archivo JSON generado para ${language} en: ${filePath}`);
+  });
+});
+
 const baseDirectory = "src/translations";
-
-// Limpiar y generar archivos JSON
-translationsArray.forEach((translation) => {
-  const { language } = translation;
-  const directoryPath = path.join(baseDirectory, language);
-  const filePath = path.join(directoryPath, "global.json");
-
-  // Asegurarse de que el directorio exista
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath, { recursive: true });
-  }
-
-  // Escribir un objeto JSON vacío en el archivo
-  fs.writeFileSync(filePath, "{}", "utf-8");
-
-  console.log(`Archivo JSON limpiado para ${language} en: ${filePath}`);
-});
-
-// Crear carpetas para cada idioma y generar archivos JSON
-translationsArray.forEach((translation) => {
-  const { language, ...rest } = translation;
-  const directoryPath = path.join(baseDirectory, language);
-  const filePath = path.join(directoryPath, "global.json");
-
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath, { recursive: true });
-  }
-
-  // Convertir datos a formato JSON
-  const jsonData = JSON.stringify({ ...rest }, null, 2);
-
-  // Escribir datos en el archivo JSON
-  fs.writeFileSync(filePath, jsonData);
-
-  console.log(`Archivo JSON generado para ${language} en: ${filePath}`);
-});
